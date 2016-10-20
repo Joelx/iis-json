021e9553a49907f400bc391e82f8d808da331de8
Schlotthauer <schlotjl@iis.fhg.de>
2016-10-20 12:08:29 +0200

    Bug gefixt, welcher bei allen Arbeiten nur bachelorarbeiten ausgegeben hat

--- like-json.php
+++ like-json.php
@@ -3,7 +3,7 @@
 /*
   Plugin Name: LIKE-Json
   Plugin URI: https://github.com/Joelx/like-json
-  Version: 1.1.1
+  Version: 1.1.3
   Description: Dient der Ausgabe von studentischen Arbeiten aus der LIKE Datenbank.
   Author: LIKE
   Author URI: 
@@ -54,7 +54,7 @@ class LIKE_Json {
     const option_name = 'like_json'; 
     const version_option_name = '_like_json_version';
     const textdomain = 'like-json';
-    const version = '1.1.1';
+    const version = '1.1.3';
     const php_version = '5.4';
     const wp_version = '4.5';
     protected static $options;
@@ -127,11 +127,9 @@ class LIKE_Json {
                 break;
             case 'arbeiten-alle':
 				$json_urls['bachelorarbeiten'] = $this->source . "bachelorarbeiten";
-                $json_urls['masterarbeiten'] = $this->source . "bachelorarbeiten";
+                $json_urls['masterarbeiten'] = $this->source . "masterarbeiten";
                 $task_object = new Studentische_Arbeiten_Alle($json_urls, $format);
                 break;
-            case 'forschungspraktika':
-                break; // TODO
             default:
                 return('Keine Übereinstimmung mit dem angegebenen Task gefunden.');
         }
