8b2df2c80c1ad15537d543bf48f0e127be7fe7b6
Joelx <joel.schlotthauer@gmail.com>
2018-08-01 22:06:40 +0200

    Version 1.4.
    -Erweiterung um Anzeige von Forschungspraktika, Projekt- und sonstigen Arbeiten.
    -Filtermethode nach "status" hinzugefügt.

--- like-json.php
+++ like-json.php
@@ -3,7 +3,7 @@
 /*
   Plugin Name: LIKE-Json
   Plugin URI: https://github.com/Joelx/like-json
-  Version: 1.3.2
+  Version: 1.4
   Description: Dient der Ausgabe von studentischen Arbeiten aus der LIKE Datenbank.
   Author: LIKE
   Author URI: 
@@ -54,7 +54,7 @@ class LIKE_Json {
     const option_name = 'like_json'; 
     const version_option_name = '_like_json_version';
     const textdomain = 'like-json';
-    const version = '1.3.2';
+    const version = '1.4';
     const php_version = '5.4';
     const wp_version = '4.5';
     protected static $options;
@@ -115,20 +115,35 @@ class LIKE_Json {
 	 *	Die einzelnen Dokumente liegen in separaten Dateien.  
 	 *  Je nach Task wird die entsprechende URL zusammengebaut.
 	 */
-    public function get_content($task = '', $id='', $format='', $advisor='') { 
+    public function get_content($task = '', $id='', $format='', $advisor='', $status='') { 
         $json_urls = array();
         switch ($task) {
             case 'masterarbeiten':
 				$json_url = $this->source . "masterarbeiten"; 
-				$task_object = new Studentische_Arbeiten($json_url, $id, $task, $format, $advisor); 
+				$task_object = new Studentische_Arbeiten($json_url, $id, $task, $format, $advisor, $status); 
 				break;
             case 'bachelorarbeiten':
 				$json_url = $this->source . "bachelorarbeiten"; 
-                $task_object = new Studentische_Arbeiten($json_url, $id, $task, $format, $advisor);
+                $task_object = new Studentische_Arbeiten($json_url, $id, $task, $format, $advisor, $status);
                 break;
+            case 'forschungspraktika':
+				$json_url = $this->source . "forschungspraktika"; 
+                $task_object = new Studentische_Arbeiten($json_url, $id, $task, $format, $advisor, $status);
+                break;	
+            case 'projektarbeiten':
+				$json_url = $this->source . "projektarbeiten"; 
+                $task_object = new Studentische_Arbeiten($json_url, $id, $task, $format, $advisor, $status);
+                break;	
+            case 'sonstiges':
+				$json_url = $this->source . "sonstiges"; 
+                $task_object = new Studentische_Arbeiten($json_url, $id, $task, $format, $advisor, $status);
+                break;						
             case 'arbeiten-alle':
 				$json_urls['bachelorarbeiten'] = $this->source . "bachelorarbeiten";
                 $json_urls['masterarbeiten'] = $this->source . "masterarbeiten";
+				$json_urls['forschungspraktika'] = $this->source . "forschungspraktika";
+				$json_urls['projektarbeiten'] = $this->source . "projektarbeiten";
+				$json_urls['sonstiges'] = $this->source . "sonstiges";
                 $task_object = new Studentische_Arbeiten_Alle($json_urls, $format);
                 break;
             default:
@@ -144,13 +159,14 @@ class LIKE_Json {
         $default = array(
             'task' => '',
             'id' => '',
-			'format' => '',
-			'advisor' => ''
+	    'format' => '',
+	    'advisor' => '',
+	    'status' => ''
         );
         $atts = shortcode_atts($default, $atts);       
         extract($atts);
         
-	    return $this->get_content($task, $id, $format, $advisor); 
+	    return $this->get_content($task, $id, $format, $advisor, $status); 
     }
 
     /*
