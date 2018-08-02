f8f6ea81f5974bcfecc1b2b61fdab471168d3edc
Joelx <joel.schlotthauer@gmail.com>
2018-08-02 16:36:37 +0200

    Update like-json.php

--- like-json.php
+++ like-json.php
@@ -3,7 +3,7 @@
 /*
   Plugin Name: LIKE-Json
   Plugin URI: https://github.com/Joelx/like-json
-  Version: 1.4
+  Version: 1.4.1
   Description: Dient der Ausgabe von studentischen Arbeiten aus der LIKE Datenbank.
   Author: LIKE
   Author URI: 
