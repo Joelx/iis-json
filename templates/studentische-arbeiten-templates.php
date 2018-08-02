81d9059c4786f1b2664df9d9b36e093838b1bfdf
Joelx <joel.schlotthauer@gmail.com>
2018-08-02 11:53:55 +0200

    Syntax Fehler verbessert

--- templates/studentische-arbeiten-templates.php
+++ templates/studentische-arbeiten-templates.php
@@ -11,19 +11,19 @@ function build_html_list($arr) {
 	
 	for ($i = 0; $i < count($arr); $i++) {
 		$output .= '<li>';
-			$output .= '<span class="list-caption">' . $arr[$i]['titel'] . '</span>'; 
+			$output .= '<span class="list-caption">' . $arr[$i]['titel'] . ' [ID: ' . $arr[$i]['id'] . ']' . '</span>'; 
 			$output .= '<ul class="list-body">';
 				$output .= '<li><b>Themenbeschreibung: </b>' . $arr[$i]['beschreibung'] . '</li>';
 				if(!empty($arr[$i]['aufgaben'])) {
 					$aufgaben = explode("/", $arr[$i]['aufgaben']);
-					$output .= "<ul">;
+					$output .= '<ul>';
 					foreach($aufgaben as $aufgabe) {
 						$output .= '<li>' . $aufgabe . '</li>';
 					}
-					$output .= "</ul>";
+					$output .= '</ul>';
 				}
 				$output .= '<li><b>Themengebiete: </b>' . $arr[$i]['kategorien'] . '</li>';
-				$output .= '<li><b>Voraussetzungen: </b>' . $arr[$i]['voraussetzungen'] . '</li>';
+				$output .= '<li><b>Voraussetzungen: </b>' . $arr[$i]['voraussetzung'] . '</li>';
 				$output .= '<li><b>Betreuer: </b>' . $arr[$i]['betreuer'] . '</li>';
 				$output .= '<li><b>Hochschullehrer: </b>' . $arr[$i]['hs_lehrer'] . '</li>';
 				if (array_key_exists('pdf', $arr[$i])) {
@@ -51,21 +51,21 @@ function build_wp_accordion($arr, $accordion_count) {
 		
 		$output .= '<div class="accordion-heading">';
 		$output .= '<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion-' . $accordion_count . '" href="#collapse_' . $collapse_count . '">';
-		$output .= $arr[$i]['titel'] . '</a>';
+		$output .= $arr[$i]['titel'] . ' [ID: ' . $arr[$i]['id'] . ']' . '</a>';
 		$output .= '</div>';
 		$output .= '<div id="collapse_' . $collapse_count . '" class="accordion-body" style="display: none;">';
 		$output .= '<ul>';
 			$output .= '<li><b>Themenbeschreibung: </b>' . $arr[$i]['beschreibung'] . '</li>';
 			if(!empty($arr[$i]['aufgaben'])) {
 					$aufgaben = explode("/", $arr[$i]['aufgaben']);
-					$output .= "<ul">;
+					$output .= '<ul>';
 					foreach($aufgaben as $aufgabe) {
 						$output .= '<li>' . $aufgabe . '</li>';
 					}
-					$output .= "</ul>";
+					$output .= '</ul>';
 				}
 			$output .= '<li><b>Themengebiete: </b>' . $arr[$i]['kategorien'] . '</li>';
-			$output .= '<li><b>Voraussetzungen: </b>' . $arr[$i]['voraussetzungen'] . '</li>';
+			$output .= '<li><b>Voraussetzungen: </b>' . $arr[$i]['voraussetzung'] . '</li>';
 			$output .= '<li><b>Betreuer: </b>' . $arr[$i]['betreuer'] . '</li>';
 			$output .= '<li><b>Hochschullehrer: </b>' . $arr[$i]['hs_lehrer'] . '</li>';
 			if (array_key_exists('pdf', $arr[$i])) {
